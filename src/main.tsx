0b8d163dd6181e7d6ed9731d735ba825c6905019
Leiladany <leiladany16@gmail.com>
2025-04-13 16:56:17 +0100

    app setup

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import "./main.css";
+import App from "./router/app";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);

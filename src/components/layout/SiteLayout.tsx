c31fd61fe490688865fa2c831915eb6546e80b73
thetiagogil <talmeidagil@gmail.com>
2026-04-27 19:07:52 +0100

    refactor: add BackgroundAccents component and corresponding styles for enhanced layout

--- src/components/layout/SiteLayout.tsx
+++ src/components/layout/SiteLayout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import { BackgroundAccents } from "./BackgroundAccents";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { useReveal } from "../../hooks/useReveal";
@@ -12,6 +13,7 @@ export function SiteLayout({ children }: SiteLayoutProps) {
 
   return (
     <div className="bg-background relative flex min-h-screen flex-col overflow-x-hidden">
+      <BackgroundAccents />
       <Header />
       <main className="relative z-10 flex-1">{children}</main>
       <Footer />
